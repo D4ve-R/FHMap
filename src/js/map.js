fbfe13cd2ca06c3984f722c0a39be01e95ec703d
D4ve-R <david.rechkemmer@googlemail.com>
2022-10-24 23:33:11 +0200

    geoJSONEntity & urlLocationEntity

--- src/js/map.js
+++ src/js/map.js
@@ -138,27 +138,6 @@ const overlays = {
     "Buildings": buildings
 };
 
-L.Map.include({
-    _initControlPos: function () {
-        const corners = this._controlCorners = {},
-        l = 'leaflet-',
-        container = this._controlContainer = L.DomUtil.create('div', l + 'control-container', this._container);
-
-        function createCorner(vSide, hSide) {
-            const className = l + vSide + ' ' + l + hSide;
-
-            corners[vSide + hSide] = L.DomUtil.create('div', className, container);
-        }
-
-        createCorner('top', 'left');
-        createCorner('top', 'right');
-        createCorner('bottom', 'left');
-        createCorner('bottom', 'right');
-        createCorner('top', 'center');
-        createCorner('bottom', 'center');
-    }
-});
-
 const map = L.map('map', {
     center: fhBaseCoords,
     zoom: zoom,
