598944ae37b1bfc17b0ee0ca121315695012e1c7
Dave <69651599+D4ve-R@users.noreply.github.com>
2022-10-25 17:53:42 +0200

    AR (#3)
    * geoJSONEntity & urlLocationEntity
    
    * clean

---
+++ src/js/aframe/urlLocation.js
@@ -0,0 +1,69 @@
+function createUrlEntity({ name, coords, primitive, color, scale }) {
+    const textScale = scale * 20;
+    const entity = document.createElement('a-entity');
+    entity.setAttribute('look-at', '[gps-projected-camera]');
+    entity.setAttribute('gps-projected-entity-place', {latitude: coords.lat, longitude: coords.lng});
+    entity.setAttribute('position', {x: 0, y: 1, z: 0});
+
+    const text = document.createElement('a-text');
+    text.setAttribute('value', name);
+    text.setAttribute('scale', {x: textScale, y: textScale, z: textScale});
+    text.setAttribute('align', 'center');
+    text.setAttribute('position', {x: 0, y: textScale/4, z: 0});
+    entity.appendChild(text);
+
+    const prim = document.createElement(primitive);
+	prim.setAttribute('scale', {x: scale, y: scale, z: scale});
+	prim.setAttribute('material', {color: color});
+	if(primitive === 'a-cone') {
+		prim.setAttribute('radius-bottom', 0.1);
+		prim.setAttribute('radius-top', 1);
+		prim.setAttribute('height', 3);
+	}
+    entity.appendChild(prim);
+    
+    return entity;
+}
+
+function getUrlParam(name){
+    if(name=(new RegExp('[?&;]'+encodeURIComponent(name)+'=([^&]*)')).exec(window.location.search))
+        return decodeURIComponent(name[1]);
+}
+
+AFRAME.registerComponent('location-url', {
+	schema: {
+		name: {
+			type: 'string',
+			default: 'Marker'
+		},
+        scale: {
+            type: 'number',
+            default: 20
+        },
+		color: {
+			type: 'string',
+			default: 'blue'
+		},
+		primitive: {
+			type: 'string',
+			default: 'a-cone'
+		}
+    },
+    init: function() {
+              const lat = getUrlParam('lat');
+              const lng = getUrlParam('lng');
+			  const title = getUrlParam('title');
+
+              if(lat != undefined && lng != undefined) {
+                  const entity = createUrlEntity({
+						name: title || this.data.name, 
+						coords: { lat: lat, lng: lng },
+						primitive: this.data.primitive,
+						color: this.data.color, 
+						scale: this.data.scale
+					});
+
+                  this.el.sceneEl.appendChild(entity);
+              }
+          }
+});
